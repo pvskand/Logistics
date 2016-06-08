c0c7c793b97bfa5420d6083d5da3a29ffa65153d
savyasachi.sa <savyasachi.sa@gmail.com>
2016-06-09 00:08:20 +0530

    Added Utility and Constants class

---
+++ app/src/main/java/com/ropar/skand/logistics/Constants.java
@@ -0,0 +1,10 @@
+package com.ropar.skand.logistics;
+
+// Add all Constants here
+
+class Constants{
+	public static String URL_GET_BOOKING_DETAILS = "bookimgDetailsUrl";
+	public static String URL_CHECK_LOGIN_CREDENTIALS = "loginCredentialsUrl";
+	// And so on
+	
+}
---
+++ app/src/main/java/com/ropar/skand/logistics/Utility.java
@@ -0,0 +1,43 @@
+package com.ropar.skand.logistics;
+
+class Utility{
+
+	public static String getBookingDetails( String usedID ){
+		// MAKE HTTP request here. and set it to response 
+
+		String response; 
+
+		// As of now  put the hardcoded JSON as response
+		response = "";
+
+		return response;
+
+	}
+
+	public static boolean checkLoginCredentials(String username, String password){
+		// MAKE HTTP POST REQUEST TO THE URL AND ASSIGN IT TO RESPONSE
+
+		String response;
+
+		// As of now 
+		response = "{ \"response\" : \"true\" }";
+
+		// Parse it to get the Boolean
+
+		Boolean bool = parseLoginResponse(response);
+
+	}
+
+	
+	pulic static boolean parseLoginResponse(String response){
+		// Parse JSON here
+
+		return true;
+	}
+
+	public parseJsonAndPopulateListView(String response){
+
+		// As the Name suggests. Though this function should be in Booking class i guess. Move it there. Call the getBookingDetails function from there and then call this there
+
+	}	
+}
